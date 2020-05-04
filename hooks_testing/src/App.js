d820507d720f964e55d36ce94b730b81aa3bd66c
Balaon <balaev_jura@mail.ru>
2020-05-04 19:13:36 +0300

    made a counter using class components

--- hooks_testing/src/App.js
+++ hooks_testing/src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-       
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      cnt: 0,
+    }
+  }
+
+  hendleClickBtn= (e)=>{
+    const { target } = e;
+    let { cnt } = this.state;
+
+    if(target.name === "up"){
+      cnt++;
+    }else{
+      cnt--;
+    }
+    this.setState({cnt});
+  }
+
+  render(){
+    return(
+      <>
+
+      <h3> count = { this.state.cnt }</h3>
+      <div>
+        <input name ="up" type="button" onClick={ this.hendleClickBtn } value="увеличить" />
+        <input name ="down" type="button" onClick={ this.hendleClickBtn } value="уменьшить" />
+      </div>
+
+      </>
+    );
+  }
+
+
+
 }
+
 
 export default App;
