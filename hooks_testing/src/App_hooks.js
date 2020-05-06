209055922b59cd06e5552e15bdb9f28e23a4a603
Balaon <balaev_jura@mail.ru>
2020-05-06 17:55:02 +0300

    add function 'hendleClickBtn'

--- hooks_testing/src/App_hooks.js
+++ hooks_testing/src/App_hooks.js
@@ -4,13 +4,21 @@ function App(){
   
   const [ cnt, setCnt ] = useState(0);
 
+  const hendleClickBtn = (e)=> {
+    const { target } = e;
+    if (target.name === "up"){
+      setCnt(cnt + 1);
+    }else{
+      setCnt( cnt - 1);
+    }
+  };
 
   return(
     <>
     <h3> count = { cnt }</h3>
       <div>
-        <input name ="up" type="button" onClick={ ()=> setCnt(cnt + 1) } value="увеличить" />
-        <input name ="down" type="button" onClick={  ()=> setCnt(cnt - 1) } value="уменьшить" />
+        <input name ="up" type="button" onClick={ hendleClickBtn } value="увеличить" />
+        <input name ="down" type="button" onClick={  hendleClickBtn } value="уменьшить" />
       </div>
     </>
   );
