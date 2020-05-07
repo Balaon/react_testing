86fafbf8cc84085afb6f9db6c8b700c039391d04
Balaon <balaev_jura@mail.ru>
2020-05-07 13:54:10 +0300

    added an example of working with arrays

--- hooks_testing/src/App_hooks.js
+++ hooks_testing/src/App_hooks.js
@@ -3,10 +3,23 @@ import React, { useState } from 'react';
 function App(){
   
   const [ cnt, setCnt ] = useState(0);
+  const [ msNum, setMsNum ] = useState(0);
   const [ userName, setUserName ] = useState("Балаев");
+  const [ ms, setMs ] = useState([1,2,3,4,5,6,7,8,9,10]);
   
+  const handleDeleteElem = (e)=>{
+    ms.splice(msNum,1)
+    //const msResult = [].concat(ms); - эквивалентно записи ниже
+    //setMs(msResult);
+    setMs([...ms]);
+  }
+
   const handleClickBtn = (e)=> {
     const { target } = e;
+    if(e.target.name ==="delete"){ 
+      handleDeleteElem();
+      return;
+    }
     if (target.name === "up"){
       setCnt(cnt + 1);
     }else{
@@ -14,9 +27,13 @@ function App(){
     }
   };
 
-  const handleChangeName = (e)=>{
+  const handleChange = (e)=>{
     const { value } = e.target;
-    setUserName(value);  
+    if(e.target.name ==="name"){
+      setUserName(value);  
+    }else{
+      setMsNum (+value)
+    }
   }
 
   return(
@@ -26,7 +43,18 @@ function App(){
         <input name ="up" type="button" onClick={ handleClickBtn } value="увеличить" />
         <input name ="down" type="button" onClick={  handleClickBtn } value="уменьшить" />
       </div>
-      <input type="text" name="name" value={ userName } onChange={ handleChangeName} />
+      <input type="text" name="name" value={ userName } onChange={ handleChange} />
+      <br/>
+      <h3>
+        {
+          ms.join(",")
+        }
+      </h3>
+      <input type="text" name="msnumber" value={ msNum } onChange={ handleChange} />
+      <input name ="delete" type="button" onClick={ handleClickBtn } value="удалить" />
+      
+      <br/>
+
     </>
   );
 }
