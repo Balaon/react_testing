4c1a88270acfc4d2c56e4a7010629894fef645e3
Balaon <balaev_jura@mail.ru>
2021-03-11 23:42:29 +0300

    import SimpleUser

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -2,28 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import { App as App2 } from "./App2";
 import { User } from './components/user'
+import { SimpleUser } from "./components/simpleUser";
 
 
 function App(props) {
+
+  const users = [{
+                    name: "Петя",
+                    age: 22,
+                    gender: "M",
+                    address: "Москва", 
+                    puppies,
+                    description: "Жили у бабуси два веселых гуся",
+                  },
+                  {
+                    name: "Надя",
+                    age: 35,
+                    gender: "F",
+                    address: "Санкт-Петербург", 
+                    puppies,
+                    description: "Немного лет тому назад, там где сливаяся, шумят",
+                  },
+                  {
+                    name: "Дима",
+                    age: 45,
+                    gender: "M",
+                    address: "Казань", 
+                    puppies,
+                    description: "Обнявшись будто две сесты, струи Арагви и Куры",
+                  },];
   return (
     <div className="App">
-      <User />
-      <div>Yo! { props.name }</div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <App2 />
+        <User description="Какое-то описание для юзера" /> <br />
+        <div style= { {display:"flex"} }>
+            <SimpleUser name="Иван" age={ 33 }/> 
+        </div>
+        <div>Yo! { props.name }</div>
+        
+        <App2 />
 
     </div>
   );
