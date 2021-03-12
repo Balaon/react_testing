aaf84093bbbc9d12c8a8af11f7a20d234848bac2
Balaon <balaev_jura@mail.ru>
2021-03-12 14:02:06 +0300

    init project bataman

---
+++ batman/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css';
+
+
+export class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+
+  render() {
+  
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+}
+
+
