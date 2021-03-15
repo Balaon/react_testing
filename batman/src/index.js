0866bac8c60e1a791f7b9bf79441101e9998ffc0
Balaon <balaev_jura@mail.ru>
2021-03-15 12:21:07 +0300

    removed not used

--- batman/src/index.js
+++ batman/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import { App } from './App';
+import  App  from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configure_store';
